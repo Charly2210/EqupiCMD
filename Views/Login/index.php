3a7184a42b4828bd831fb2004df238e7aee2fce3
Charly2210 <isc_palma.j@tesvb.edu.mx>
2018-11-07 09:38:47 -0600

    MODIFICACIONES

--- Views/Login/index.php
+++ Views/Login/index.php
@@ -92,25 +92,50 @@
 <html>
 <body>
 
-<div class="login-form">
-    <form action="/examples/actions/confirmation.php" method="post">
+<div class="container center">
+    <div>
+        <h5 class="red-text">
+            <?php echo isset($_SESSION["error_login"])?$_SESSION["error_login"]:""?>
+        </h5>
+    </div>
+    <div class="container">
+        <h1 class="form-heading">login Form</h1>
+        <div class="login-form">
+            <div class="main-div">
+                <form method="post" action="<?php echo URL?>login/verify">
+                    <div class="panel">
+                        <h3>Inicio de Sesión</h3>
+                        <p>Por favor ingresa tu e-mail y contraseña</p>
+                    </div>
+                    <div class='form-group'>
+                        <div class='input-field col s12'>
+                            <input class='validate' type='email' name='email' id='email'>
+                        </div>
+                    </div>
 
-        <h2 class="text-center">Inicio de sesion</h2>
-        <div class="form-group">
-            <input type="text" class="form-control" name="username" placeholder="Username" required="required">
-        </div>
-        <div class="form-group">
-            <input type="password" class="form-control" name="password" placeholder="Password" required="required">
-        </div>
-        <div class="form-group">
-            <button type="submit" class="btn btn-primary btn-lg btn-block">Entrar</button>
-        </div>
-        <div class="clearfix">
-            <label class="pull-left checkbox-inline"><input type="checkbox"> Recordarme</label>
-            <a href="#" class="pull-right"> Olvidaste tu contraseña?</a>
-        </div>
-    </form>
+                    <div class='form-group'>
+                        <div class='input-field col s12'>
+                            <input class='validate' type='password' name='password' id='password'>
+                        </div>
+                    </div>
 
+                    <br />
+                    <center>
+                        <div class='row'>
+                            <button type='submit' name='btn_login'><b>Entrar</b></button>
+                        </div>
+
+                    </center>
+                </form>
+            </div>
+        </div>
+    </div>
 </div>
+
+<script type="text/javascript">
+    $(document).ready(function(){
+        Materialize.updateTextFields();
+    });
+</script>
 </body>
 </html>
--- Views/Template.php
+++ Views/Template.php
@@ -16,17 +16,20 @@ class Template
             <link href="<?php echo URL?>/Public/css/bootstrap.css" type="text/css" rel="stylesheet">
             <link href="<?php echo URL?>/Public/css/bootstrap-reboot.css" type="text/css" rel="stylesheet">
             <link rel="stylesheet" href="<?php echo URL?>/Public/style.css">
+            <link rel="stylesheet" href="<?php echo URL?>/Public/Estilologin.css"
             <script type="text/javascript" src="<?php echo URL?>/Public/js/bootstrap.bundle.min.js"></script>
             <script type="text/javascript" src="<?php echo URL?>/Public/js/bootstrap.js"></script>
             <script type="text/javascript" src="<?php echo URL?>/Public/js/jquery.min.js"></script>
+
+
+
 
 
             <link href="css/full-slider.css" rel="stylesheet">
         </head>
 
 
-
-
+        <body>
         <main>
 
             <title>Biblioteca Digital </title>
@@ -63,15 +66,17 @@ class Template
         </html>
         <?php
     }
-
     public function footer(){
         ?>
         <footer class="container">
-            <p class="float-right"><a href="#">Back to top</a></p>
-            <p>&copy; 2017-2018 Company, Inc. &middot; <a href="#">Privacy</a> &middot; <a href="#">Terms</a></p>
+            <nav class="navbar navbar-expand-lg navbar-dark  fixed-bottom bg-dark ">
+                <div class="container">
+                    <p class="text-left text-white">Copyright &copy; Derechos Reservados BIBLIOTEC</p>
+                </div>
+            </nav>
+
         </footer>
         <?php
 
     }
 }
-
